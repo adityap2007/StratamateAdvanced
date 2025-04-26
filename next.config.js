c13f069899761b60bb2fdfd54f04deef6dfdf119
apet0210 <apet0210@uni.sydney.edu.au>
2025-04-26 10:39:00 +1000

    Fix API route handling and Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 // Trigger new deployment
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client')
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig 
